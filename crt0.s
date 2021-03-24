fe37dbfb2c9eec005131e8b5893a8ec672d71e12
Vivien B <vivienbihl@gmail.com>
2021-03-24 20:44:31 +0100

    Try interrupt stuff

--- crt0.s
+++ crt0.s
@@ -1,6 +1,62 @@
+.ARM
+
 _start:
     b header_end
     .space 188
 header_end:
+    bl registerVblank
+
+    /* call AgbMain in thumb mode */
     ldr r0, =AgbMain
     bx r0
+
+.ARM
+interruptHandler:
+    /* only vblank interrupt is enabled */
+    /* so it's pretty straight forward */
+    /* acknowledge REG_IF (4000202) */
+    mov r1, #0x4000000
+    mov r2, #1
+    str r2, [r1, #0x202]
+
+    /* acknowledge REG_IFBIOS (4000202) */
+    /* to be able to use swi 5 */
+    str r2, [r1, #-8]
+
+    /* ldr r3, [r1, #-8] */
+    /* ldr r2, [r3] */
+    /* orr r2, #1 */
+    /* str r2, [r1, #-8] */
+
+    bx lr
+
+.ARM
+.global registerVblank
+registerVblank:
+    /* enable v-blank interrupt */
+    mov r1, #0x4000000
+
+    /* REG_IME (4000208) = 0, disable all interrupts */
+    mov r2, #0
+    str r2, [r1, #0x208]
+
+    /* REG_INTERRUPT (3007FFC) = interruptHandler */
+    ldr r2, =interruptHandler
+    str r2, [r1, #-4]
+
+    /* REG_DIPSTAT (4000004) |= 8 (4th bit -> enable vblank) */
+    ldr r2, [r1, #4]
+    orr r2, #8
+    str r2, [r1, #4]
+
+    /* REG_IE (4000200) |= 1 (1st bit -> enable vblank) */
+    ldr r2, [r1, #0x200]
+    orr r2, #1
+    str r2, [r1, #0x200]
+
+    bx lr
+
+.THUMB
+.global vblankWait
+vblankWait:
+    swi 5
--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,19 @@ fn clear_screen() {
 extern "C" fn AgbMain() -> ! {
   clear_screen();
 
-  write_vram(0xf1, 240 * 160 / 2 + 240 / 2);
   write_io(0x0403, 0);
 
-  loop {}
+  let mut i:u16 = 0;
+
+  loop {
+    write_vram(i, 240 * 160 / 2 + 240 / 2);
+    i ^= 0xffff;
+
+    unsafe { vblankWait(); }
+  }
+}
+
+#[no_mangle]
+extern "C" {
+  fn vblankWait();
 }
