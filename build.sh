e98f617f5fe0731c87120b3760f1e00f436f1bbf
Vivien B <vivienbihl@gmail.com>
2021-04-07 20:53:17 +0200

    Use default target file

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p target
 arm-none-eabi-as crt0.s -o target/crt0.o || exit 1
-cargo xbuild --release --target thumbv4-none-agb.json || exit 1
-arm-none-eabi-objcopy -O binary target/thumbv4-none-agb/release/gba-demo /mnt/c/Users/vbihl/Desktop/issou.gba || exit 1
+cargo xbuild --release --target thumbv4t-none-eabi.json || exit 1
+arm-none-eabi-objcopy -O binary target/thumbv4t-none-eabi/release/gba-demo /mnt/c/Users/vbihl/Desktop/issou.gba || exit 1
